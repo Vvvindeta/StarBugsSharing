63c5eae670228c82719b452fef7854748b2f6855
tiredtiredness <tiredtiredness@gmail.com>
2023-12-05 23:14:44 +0300

    Update database in settings and add .gitignore

--- StarBugsSharing/StarBugsSharing/settings.py
+++ StarBugsSharing/StarBugsSharing/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from configs import db_password
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +77,15 @@ WSGI_APPLICATION = 'StarBugsSharing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'StarBugsSharing',
+        'USER': 'root',
+        'PASSWORD': db_password,
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
