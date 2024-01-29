2977cb1a71f7bcc1bcd9fa3c5038765f180287a2
Hogen <dimal2003play@gmail.com>
2024-01-29 20:09:55 +0300

    filters

--- StarBugsSharing/rovers/views.py
+++ StarBugsSharing/rovers/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 
 from rovers.models import Rover
 
+
 def rent(request):
+    # Получаем список марсоходов
+    rovers = Rover.objects.all()
+    # Получаем параметры сортировки из запроса
+    sort_by = request.GET.get('sort_by', 'default')
+    if sort_by == 'rate':
+        rovers = rovers.order_by('rate')
+    elif sort_by == 'mileage':
+        rovers = rovers.order_by('mileage')
+    elif sort_by == 'speed':
+        rovers = rovers.order_by('speed')
 
     context = {
-        "rovers": Rover.objects.all(),
+        'rovers': rovers,
     }
-
     return render(request, 'rovers/rent.html', context)
