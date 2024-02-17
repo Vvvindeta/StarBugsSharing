b6a020ab16ecc52929dfc7aed00c75f94dc4e4af
tiredtiredness <tiredtiredness@gmail.com>
2024-02-18 01:28:09 +0300

    Accordion bugfix

--- StarBugsSharing/static/js/script.js
+++ StarBugsSharing/static/js/script.js
@@ -1,23 +1,3 @@
-const accordionBtns = document.querySelectorAll('.accordion-btn')
-accordionBtns.forEach(btn => {
-    btn.addEventListener('click', function () {
-        this.classList.toggle('active')
-        const accordionDescription = this.nextElementSibling
-        const plusIcon = this.querySelector('.plus-icon')
-        const minusIcon = this.querySelector('.minus-icon')
-        console.log(accordionDescription)
-        if(accordionDescription.style.height) {
-            accordionDescription.style.height = null
-            plusIcon.style.display = 'block'
-            minusIcon.style.display = 'none'
-        } else {
-            accordionDescription.style.height = accordionDescription.scrollHeight + 'px'
-            plusIcon.style.display = 'none'
-            minusIcon.style.display = 'block'
-        }
-    })
-})
-
 document.addEventListener('DOMContentLoaded', function() {
     var slogans = document.querySelectorAll('.hero-slogan h1');
     var link = document.querySelector('.hero-link');
